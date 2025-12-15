3f9305ecbedf767262d5a45d5175f7f7dccf195f
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-12-15 13:16:57 +0200

    init

---
+++ MysticDraw/App/MysticDrawApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticDrawApp.swift
+//  MysticDraw
+//
+//  Created by Serhii Babchuk on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MysticDrawApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MDLaunchView()
+        }
+    }
+}
